9a9dfe0d87335b0a9592dbf55c759a6f862d9612
Andrey Nedobylsky <admin@twister-vl.ru>
2022-05-19 14:22:25 +0200

    Dark mode adapt

--- plugins/nft_wallet/modules/frameModule.mjs
+++ plugins/nft_wallet/modules/frameModule.mjs
@@ -79,7 +79,7 @@ async function showCollections(collections) {
                     </div>
                 </section>
         `;
-        html += `    <div class="album py-5 bg-light">
+        html += `    <div class="album py-5 ">
         <div class="container">
 
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
