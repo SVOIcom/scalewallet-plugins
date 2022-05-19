cd83c5d4ae9abda601e39a00bc1fd6d84ceb4e4f
Andrey Nedobylsky <admin@twister-vl.ru>
2022-05-19 13:50:38 +0200

    Start NFT wallet

--- plugins/nft_wallet/modules/frameModule.mjs
+++ plugins/nft_wallet/modules/frameModule.mjs
@@ -5,6 +5,7 @@ import {
 } from "https://everscale-connect.svoi.dev/everscale/0.1.3/getProvider.mjs";
 import TIP4Collection from "https://everscale-connect.svoi.dev/everscale/contracts/TIP4Collection.mjs";
 import CONSTS from "./consts.mjs";
+import pluginUtils from "../../../lib/pluginUtils.mjs";
 
 window.getProvider = getProvider;
 window.PROVIDERS = PROVIDERS;
@@ -60,4 +61,67 @@ async function collectCollections(userAddress, collectionsList = CONSTS.NFT_COLL
     return collections;
 }
 
-console.log(await collectCollections(userAddress));
+async function showCollections(collections) {
+    let html = '';
+    for (let collectionName in collections) {
+        let collection = collections[collectionName];
+        html += `
+                <section class=" text-center container">
+                    <div class="row align-items-center">
+                        <div class="col-3">
+                           <div>
+                                <img src="${collection.preview?.source}" class="collectionImg">
+                            </div>
+                        </div>
+                        <div class="col-7 align-middle">
+                            <h1 class="fw-light collectionTitle">${collectionName}</h1>
+                        </div>
+                    </div>
+                </section>
+        `;
+        html += `    <div class="album py-5 bg-light">
+        <div class="container">
+
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        `;
+
+        for (let token of collection.tokens) {
+            console.log(token);
+            html += `
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <div>
+                            <img src="${token.preview?.source}" class="tokenImage card-img"> 
+                        </div>
+                        <div class="card-body">
+                            <h5 class="card-title">${token.name ? token.name : '#' + Number(token.id)}</h5>
+                            <p class="card-text">${token?.description}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Transfer</button>
+                                  <!--  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button> -->
+                                </div>
+                                <small class="text-muted autoClipboard nftAddress" data-clipboard="${token.address}">${UTILS.shortenPubkey(token.address)}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `;
+        }
+
+        html += `    </div>
+        </div>
+    </div>`;
+    }
+
+    console.log(html);
+
+    $('#pluginMain').html(html);
+    $('.autoClipboard').click(pluginUtils.selfCopyElement());
+}
+
+let userCollections = await collectCollections(userAddress);
+
+console.log(userCollections);
+
+await showCollections(userCollections);
